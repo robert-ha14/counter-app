b9cdcf9e2a5d77883aa1fb3c6474bfb1e6caba1f
bobha14 <bobha14@gmail.com>
2020-09-11 22:03:07 +0900

    m1 files

---
+++ Downloads/M1/src/main/Person4.java
@@ -0,0 +1,41 @@
+package main;
+
+/**
+ *  TODO: 
+ *  1. Change the author to your full name
+ *  2. Follow the tutorial located here: https://www.tutorialspoint.com/junit/junit_environment_setup.htm
+ *       to setup your junit environment and begin creating test cases.
+ *  3. Create a new directory for your tests located at M1/src/tests/person4
+ *  4. Now create a TestPerson4 Class and write methods to test add, subtract, multiply and divide
+ *  5. Create a TestRunner Class and verify your tests run successfully.
+ *  Reminder: Each individual is to complete this TODO
+ *
+ *  @author Bob
+ *  @version 1.1
+ */
+public class Person4 {
+
+    private String name;
+
+    public Person4(String name) {
+        this.name = name;
+    }
+
+    public long add(long first, long second) {
+        return first + second;
+    }
+
+    public long subtract(long first, long second) {
+        return first - second;
+    }
+
+    public long multiply(long first, long second) {
+        return first * second;
+    }
+
+    public long divide(long first, long second) {
+        if (second == 0L)
+            throw new IllegalArgumentException("Cannot divide by zero!");
+        return first / second;
+    }
+}
